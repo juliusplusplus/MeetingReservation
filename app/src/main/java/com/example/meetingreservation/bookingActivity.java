c80d6cd1f181aea64ff09258e359b45812897c71
juliusplusplus <2985869424@qq.com>
2019-03-18 16:30:11 +0800

    1.2版本：备份，试图利用不同网上已有demo综合实现所需时间段选择

--- app/src/main/java/com/example/meetingreservation/bookingActivity.java
+++ app/src/main/java/com/example/meetingreservation/bookingActivity.java
@@ -1,8 +1,19 @@
 package com.example.meetingreservation;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
 /**
  * Created by Jay on 2019/3/12.
  */
 
-public class bookingActivity {
+public class bookingActivity extends AppCompatActivity {
+    private ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+        //back.setOnClickListener(this);
+    }
 }
